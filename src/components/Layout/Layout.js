ab2346f6ee72eb753f3eba136eea8a7ad3377e80
jun <wanghaijun04@gmail.com>
2019-04-09 16:20:15 +1000

    made basic layout for note

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Layout = ()=>{
+
+  return (
+    <React.Fragment>
+    <Header />
+    <main>notes go here</main>
+    </React.Fragment>
+  );
+}
+
+export default Layout;
