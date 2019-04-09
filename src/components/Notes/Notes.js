96c07efa35cb114b5800f2d7a494344bd5bc33fa
jun <wanghaijun04@gmail.com>
2019-04-10 00:13:12 +1000

    built up notes component using mui grid

---
+++ src/components/Notes/Notes.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Note from './Note/Note';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme=> ({
+  root: {
+    maxWidth:'100%',
+    margin:'auto',
+    justifyContent:'center',
+    [theme.breakpoints.up('md')]: {
+      maxWidth:'85%',
+      justifyContent:'flex-start',
+    },
+  },
+  item: {
+    [theme.breakpoints.down('md')]: {
+      flexGrow: 1,
+    }
+  }
+})
+
+const Notes = ({info,classes})=>{
+
+  const notes = info.map((item)=>
+    <Grid key={item.id} item xs={12} md={6} lg={4} className={classes.item} >
+      <Note singleInfo={item} />
+    </Grid>
+  );
+
+  return (
+    <Grid container spacing={40} className={classes.root} >
+      {notes}
+    </Grid>
+  );
+}
+
+export default withStyles(styles)(Notes);
