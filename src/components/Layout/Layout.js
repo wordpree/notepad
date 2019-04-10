b1e230db1419e5bcf757abb71ffc8e6673c44cf3
jun <wanghaijun04@gmail.com>
2019-04-10 23:30:55 +1000

    moved Form component from layout to notebuild

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../Header/Header';
-import Form from '../Form/Form';
 import NoteBuild from '../../controller/NoteBuild';
 
 const Layout = ()=>{
@@ -12,7 +11,6 @@ const Layout = ()=>{
     <React.Fragment>
       <Header />
       <main className="content-entry" style={style} >
-        <Form />
         <NoteBuild />
       </main>
     </React.Fragment>
