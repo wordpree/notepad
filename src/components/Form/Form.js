9e5215663837fa50898aabc0dbd271ba19f69dff
jun <wanghaijun04@gmail.com>
2019-04-10 23:26:39 +1000

    muilti textField refactored && form submit added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const styles= theme => ({
   form: {
@@ -15,35 +16,55 @@ const styles= theme => ({
   typography: {
     fontSize:'1.5rem',
     marginTop: '1rem',
+  },
+  input: {
+    display: 'none'
+  },
+  button: {
+    color:'#303f9f',
+    margin:'1.5rem auto',
+    '&:hover': {
+      backgroundColor:'#303f9f',
+      color:'white'
+    }
   }
 });
-const Form = ({classes})=>{
+const Form = ({classes,onchange,onsubmit})=>{
 
   return (
     <React.Fragment>
-      <Typography component='h2' paragraph={true} align='center' className={classes.typography}>
+      <Typography
+            component='h2'
+            paragraph={true}
+            align='center'
+            className={classes.typography}
+      >
         Compose Your Note Below
       </Typography>
-      <form className={classes.form}>
-        <TextField
-            className={classes.textField}
-            id="note-title"
-            label="Title"
-            margin="dense"
-        />
-        <TextField
-            className={classes.textField}
-            id="note-name"
-            label="Name"
-            margin="dense"
-        />
-        <TextField
-            className={classes.textField}
-            fullWidth = {true}
-            id="note-content"
-            label="content"
-            margin="dense"
-        />
+      <form className={classes.form} onSubmit={onsubmit}>
+        {
+          ['heading','author','content'].map(
+            (item)=><TextField
+                  className={classes.textField}
+                  key ={item}
+                  id={item}
+                  label={item}
+                  margin="dense"
+                  fullWidth={item==='content'}
+                  onChange = {onchange}
+                />
+          )
+        }
+        <input
+        id="note-submit"
+        type="submit"
+        className={classes.input}
+      />
+      <label htmlFor="note-submit">
+        <Button variant="outlined" component="span" className={classes.button}>
+          add new
+        </Button>
+      </label>
       </form>
     </React.Fragment>
   );
